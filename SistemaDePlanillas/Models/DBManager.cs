aef9f87824d1f7809302f414ce0d2c6dfbeea89c
chavesandres1009 <andresch1009@gmail.com>
2016-02-21 00:25:46 -0600

    Prueba3
    Prueba3

--- SistemaDePlanillas/Models/DBManager.cs
+++ SistemaDePlanillas/Models/DBManager.cs
@@ -12,11 +12,12 @@ namespace SistemaDePlanillas.Models
 
 
         // prueba2
+        // prueba3
         private static DBManager single;
         private NpgsqlConnection cnx;
-        // private string stringConnect = "Server=localhost;Port=5432;Database=COOPESUPERACION;User Id=postgres;Password=root;";
+        private string stringConnect = "Server=localhost;Port=5432;Database=COOPESUPERACION;User Id=postgres;Password=root;";
         // Francisco
-        private string stringConnect = "Server=localhost;Port=5432;Database=planillas;User Id=postgres;Password=postgres;";
+        // private string stringConnect = "Server=localhost;Port=5432;Database=planillas;User Id=postgres;Password=postgres;";
         private string OK = "OK";
 
         private DBManager() { }
