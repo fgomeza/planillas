a81a0c00349120c1ed166e74d000c77300bf8ec8
JafetRoman <jafet21@hotmail.es>
2016-02-21 00:13:15 -0600

    reunion

---
+++ SistemaDePlanillas/Models/Operations/DebitsGroup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaDePlanillas.Models.Operations
+{
+    public class DebitsGroup
+    {
+        static string add1()
+        {
+
+            var res=DBManager.getInstance().addExtra(1, "detalle", 42525252);
+            if (res.status == 0)
+            {
+                return "{status:'OK'}";
+            }
+            else
+            {
+                return "{status:'ERROR', error:43141, detail:'Paso algo grave :('}";
+            }
+        }
+    }
+}
