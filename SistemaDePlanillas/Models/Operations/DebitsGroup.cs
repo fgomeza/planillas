dc49f131fc0674052aa98f22b3fcf01c146bb4b7
Carlos Aguero B <calostaq@gmail.com>
2016-02-22 14:16:18 -0600

    Mensajes
    Intento de mejorar el flujo de mensajes serializando objetos c# a
    objetos JSON

--- SistemaDePlanillas/Models/Operations/DebitsGroup.cs
+++ SistemaDePlanillas/Models/Operations/DebitsGroup.cs
@@ -2,31 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
 
 namespace SistemaDePlanillas.Models.Operations
 {
     public class DebitsGroup
     {
-<<<<<<< HEAD
         public static string AddFixedDebit(User user, int employee, string detail, int amount)
         {
 
-            var response = DBManager.getInstance().addDebit(employee, detail, amount);
-            if (response.status == 0)
-=======
-        public static string add1(User user, long employee, string detail, long amount)
-        {
-
-            var res = DBManager.getInstance().addDebit((int)employee, detail, amount);
-            if (res.status == 0)
->>>>>>> refs/remotes/origin/master
-            {
-                return "{status:'OK'}";
-            }
-            else
-            {
-                return "{status:'ERROR', error:43141, detail:'Paso algo grave :('}";
-            }
+            var dataBaseResponse = DBManager.getInstance().addDebit(employee, detail, amount);
+            return new JavaScriptSerializer().Serialize(dataBaseResponse);
+   
         }
 
         public static string test(User user, string texto, long numero, double dec)
---
+++ SistemaDePlanillas/Models/Operations/ExtraGroup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaDePlanillas.Models.Operations
+{
+    public class ExtraGroup
+    {
+        public string AddExtras()
+        {
+            return "";
+        }
+        public string RemoveExtras()
+        {
+            return "";
+        }
+        public string UpdateExtras()
+        {
+            return "";
+        }
+    }
+}
