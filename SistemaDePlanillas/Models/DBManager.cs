e69e92fcd4baeb9e61613923a3332d5e95511b28
Carlos Aguero B. <calostaq@gmail.com>
2016-02-21 18:37:17 -0600

    Se eliminaron comentarios de prueba

--- SistemaDePlanillas/Models/DBManager.cs
+++ SistemaDePlanillas/Models/DBManager.cs
@@ -9,15 +9,7 @@ namespace SistemaDePlanillas.Models
 {
     public class DBManager
     {
-        //comentario 2
-        //comentario 3
 
-<<<<<<< HEAD
-=======
-
-        // prueba2
-        // prueba3
->>>>>>> origin/master
         private static DBManager single;
         private NpgsqlConnection cnx;
         private string stringConnect = "Server=localhost;Port=5432;Database=COOPESUPERACION;User Id=postgres;Password=root;";
