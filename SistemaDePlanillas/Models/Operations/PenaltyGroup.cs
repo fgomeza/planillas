42cb17a4a5c1d12308ba1cfca414c212e440d307
Carlos Aguero B. <calostaq@gmail.com>
2016-02-23 13:46:04 -0600

    se añadio penaltyGroup

---
+++ SistemaDePlanillas/Models/Operations/PenaltyGroup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaDePlanillas.Models.Operations
+{
+    public class PenaltyGroup
+    {
+        public string AddPenalty()
+        {
+            return "";
+        }
+        public string RemovePenalty()
+        {
+            return "";
+        }
+        public string UpdatePenalty()
+        {
+            return "";
+        }
+        public string ListAllPenaltys()
+        {
+            return "";
+        }
+        public string FindPenaltyById(int id)
+        {
+            return "";
+        }
+    }
+}
