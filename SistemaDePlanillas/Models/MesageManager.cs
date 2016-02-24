a24be3e1fb9bbf660cf6fac8d2c88284ce2c950a
Carlos Aguero B. <calostaq@gmail.com>
2016-02-23 18:07:48 -0600

    se añadio el enum TypeStatus

--- SistemaDePlanillas/Models/MesageManager.cs
+++ SistemaDePlanillas/Models/MesageManager.cs
@@ -10,7 +10,7 @@ namespace SistemaDePlanillas.Models
     /// Esta clases gestiona el lanzamiendo de mensajes entre modulos 
     /// hace a la ves de repositorio comun de errores
     /// </summary>
-    public class ErrorManager
+    public class MesageManager
     {
         private static Dictionary<int, Message> reposotory = new Dictionary<int, Message>();
         private static JavaScriptSerializer serializer = new JavaScriptSerializer();
@@ -23,10 +23,10 @@ namespace SistemaDePlanillas.Models
         {
             /*Esto es un pseudo origen de datos*/
             List<Message> ErrorList = new List<Message>();
-            ErrorList.Add(new Message(1, "Error tipo 1"));
-            ErrorList.Add(new Message(2, "Error tipo 2"));
-            ErrorList.Add(new Message(3, "Error tipo 3"));
-            ErrorList.Add(new Message(4, "Error tipo 4"));
+            ErrorList.Add(new Message(1,0, "Error tipo 1"));
+            ErrorList.Add(new Message(2,0, "Error tipo 2"));
+            ErrorList.Add(new Message(3,0, "Error tipo 3"));
+            ErrorList.Add(new Message(4,1, "Error tipo 4"));
             /*de aca consulta a la base de datos*/
             foreach (Message message in ErrorList)
                 reposotory.Add(message.Code, message);
@@ -66,17 +66,17 @@ namespace SistemaDePlanillas.Models
         private string _details;
         private int _code;
 
-
-        public Message(int code, string details)
+        public Message(int code, TypeStatus status, string details)
         {
             Code = code;
-            Status = "Error";
+            Status = status.ToString();
             Details = details;
         }
-        public Message(int code,string status, string details)
+
+        public Message(int code, int status, string details)
         {
             Code = code;
-            Status = status;
+            Status = ((TypeStatus)status).ToString();
             Details = details;
         }
         public int Code
@@ -96,6 +96,15 @@ namespace SistemaDePlanillas.Models
             get { return _details; }
             set { _details = value; }
         }
+       
 
+    }
+    /// <summary>
+    /// Enum que hace de representacion numerica del estado de un mensaje
+    /// </summary>
+    public enum TypeStatus
+    {
+        Error,
+        success
     }
 }
