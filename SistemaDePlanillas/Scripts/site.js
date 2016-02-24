93fe15884c0080d12dcea33cc7808f600f01da8e
JafetRoman <jafet21@hotmail.es>
2016-02-23 19:08:55 -0600

    dasf

--- SistemaDePlanillas/Scripts/site.js
+++ SistemaDePlanillas/Scripts/site.js
@@ -12,6 +12,18 @@ var action = function (group, operation, args, callback) {
     });
 }
 
+var action = function (group, operation, method, args, callback) {
+    $.ajax({
+        url: "/api/action/" + group + "/" + operation+"/"+method,
+        type: "POST",
+        data: JSON.stringify(args),
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: function (data) { callback(JSON.parse(data)); },
+        error: function () { callback({ status: 'ERROR', error: 404, detail: 'Sin respuesta del servidor' }); }
+    });
+}
+
 var actionString = function (group, operation, args, callback) {
     args = "[" + args + "]";
     var x = JSON.parse(args);
