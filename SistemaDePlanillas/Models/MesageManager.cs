23aebf6e84cbf567ee5adb49f0df48cf8a993927
Carlos Aguero B. <calostaq@gmail.com>
2016-02-23 19:45:11 -0600

    Se elimino funcion no utilizada

--- SistemaDePlanillas/Models/MesageManager.cs
+++ SistemaDePlanillas/Models/MesageManager.cs
@@ -54,7 +54,6 @@ namespace SistemaDePlanillas.Models
             Message error = (reposotory.ContainsKey(code)) ? reposotory[code] : null;
             return (error == null) ? "{status:'Error no encontrado'}" : Serializer.Serialize(error);
         }
-        public static generateMessage()
     }
 
     /// <summary>
