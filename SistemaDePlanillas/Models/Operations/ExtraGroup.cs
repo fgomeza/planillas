c718a7eb717e898fb2409b893539d2d4941f772a
Carlos Aguero B <calostaq@gmail.com>
2016-02-24 16:47:08 -0600

    Se agregaron los modulos extra y penalty

---
+++ SistemaDePlanillas/Models/Operations/ExtraGroup.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+namespace SistemaDePlanillas.Models.Operations
+{
+    /// <summary>
+    /// Modulo para manteminiemto de horas extras
+    /// </summary>
+    public class ExtraGroup
+    {
+        /// <summary>
+        /// Genera un pago extra y lo asocia a un empleado
+        /// </summary>
+        /// <param name="employeeId">identificador del empleado</param>
+        /// <param name="detail">Motivo del pago</param>
+        /// <param name="amount">Cantidad a pagar</param>
+        /// <returns>Estado de la transaccion en formato JSON</returns>
+        public string AddExtras(long employeeId, string detail,float amount)
+        {
+            try
+            {
+                DBManager.getInstance().addExtra((int)employeeId, detail, amount);
+                return "";
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+        /// <summary>
+        /// Elimina un pago extra
+        /// </summary>
+        /// <param name="extraId">identificador del pago extra</param>
+        /// <returns>Estado de la transaccion en formato JSON</returns>
+        public string RemoveExtras(long extraId)
+        {
+            try
+            {
+                DBManager.getInstance().deleteExtra((int)extraId);
+                return "";
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+        /// <summary>
+        /// Actualiza la informacio de un pago extra
+        /// </summary>
+        /// <param name="extraId">Identificador del pago extra</param>
+        /// <param name="detail">Motivo del pago</param>
+        /// <param name="amount">Cantidad a pagar</param>
+        /// <returns>Estado de la transaccion en formato JSON</returns>
+        public string UpdateExtras(long extraId, string detail, float amount)
+        {
+            try
+            {
+                DBManager.getInstance().updateExtra((int)extraId, detail, amount);
+                return "";
+            }
+            catch (Exception)
+            {
+
+                return "";
+            }
+            
+        }
+        /// <summary>
+        /// Accesa los pagos extra asociados a un empleado
+        /// </summary>
+        /// <param name="employeeId">Identificador del empleado</param>
+        /// <returns>
+        /// Lista con los cagos extra asociados al empleado
+        /// Estado de la transaccion en formato JSON
+        /// </returns>
+        public string ListAllExtras(long employeeId)
+        {
+            try
+            {
+                List<Extra> allExtras = DBManager.getInstance().selectExtras((int)employeeId).detail;
+                return MessageManager.Serializer.Serialize(allExtras);
+            }
+            catch (Exception)
+            {
+
+                return "";
+            }
+      
+        }
+        /// <summary>
+        /// Accesa a un pago por su identificador
+        /// </summary>
+        /// <param name="extraId">Identificador del pago extra</param>
+        /// <returns>
+        /// Pago extra asociado al identificador
+        /// Estado de la transaccion en formato JSON
+        /// </returns>
+        public string FindExtraById(long extraId)
+        {
+            try
+            {
+                Extra extra = DBManager.getInstance().selectExtra((int)extraId).detail;
+                return MessageManager.Serializer.Serialize(extra);
+            }
+            catch (Exception)
+            {
+                return "";
+                
+            }
+        }
+    }
+}
---
+++ SistemaDePlanillas/Models/Operations/PenaltyGroup.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaDePlanillas.Models.Operations
+{
+    /// <summary>
+    /// Modulo de mantenimiento de penalizaciones
+    /// </summary>
+    public class PenaltyGroup
+    {
+        /// <summary>
+        /// Genera una penalizacion y la asocia a un empleado
+        /// </summary>
+        /// <param name="employeeId">Identificador del empleado</param>
+        /// <param name="detail">Motivo de la penalizacion</param>
+        /// <param name="amount">Monto a descontar</param>
+        /// <returns>Estado de la transaccion en formato JSON</returns>
+        public string AddPenalty(long employeeId, string detail, float amount)
+        {
+            try
+            {
+                //DBManager.getInstance().addPenalty(employeeId, detail, amount);
+                return "";
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+        /// <summary>
+        /// Elimina una penalizacion asociada a un empleado
+        /// </summary>
+        /// <param name="penaltyId">Identificador de la penalizacion</param>
+        /// <returns>>Estado de la transaccion en formato JSON</returns>
+        public string RemoveExtras(long penaltyId)
+        {
+            try
+            {
+                //DBManager.getInstance().deletePenalty((int)penaltyId);
+                return "";
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+        /// <summary>
+        /// Actualiza la informacion de una penalizacion
+        /// </summary>
+        /// <param name="penaltyId">Identificador de la penalizacion</param>
+        /// <param name="detail">Motivo de la penalizacion</param>
+        /// <param name="amount">Monto a descontar</param>
+        /// <returns>Estado de la transaccion en formato JSON</returns>
+        public string UpdateExtras(long penaltyId, string detail, float amount)
+        {
+            try
+            {
+                //DBManager.getInstance().updatePenalty((int)penaltyId, detail, amount);
+                return "";
+            }
+            catch (Exception)
+            {
+
+                return "";
+            }
+
+        }
+        /// <summary>
+        /// Accesa a las penalizaciones vinculadas a un empleado
+        /// </summary>
+        /// <param name="employeeId">Identificador del empleado</param>
+        /// <returns>
+        /// Lista con las penalizaciones asociadas al usuario
+        /// Estado de la transaccion en formato JSON
+        /// </returns>
+        public string ListAllPenalties(long employeeId)
+        {
+            try
+            {
+                //List<Extra> allExtras = DBManager.getInstance().selectPenalties((int)employeeId).detail;
+                //return MessageManager.Serializer.Serialize(allExtras);
+                return "";
+            }
+            catch (Exception)
+            {
+
+                return "";
+            }
+
+        }
+        /// <summary>
+        /// Accesa a una penalizacion por su identificador
+        /// </summary>
+        /// <param name="extraId">Identificador de la penalizacion</param>
+        /// <returns>
+        /// Penalizacion asociada al identificador
+        /// Estado de la transaccion en formato JSON
+        /// </returns>
+        public string FindPenaltyById(long extraId)
+        {
+            try
+            {
+                //Extra extra = DBManager.getInstance().selectPenalty((int)extraId).detail;
+                //return MessageManager.Serializer.Serialize(extra);
+                return "";
+            }
+            catch (Exception)
+            {
+                return "";
+
+            }
+        }
+    }
+}
