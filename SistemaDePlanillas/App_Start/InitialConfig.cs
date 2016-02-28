0a9f86586e378fdd31351cf62e212b467e368fa5
JafetRoman <jafet21@hotmail.es>
2016-02-28 14:45:08 -0600

    inicia los singletones al iniciar

---
+++ SistemaDePlanillas/App_Start/InitialConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SistemaDePlanillas.Models;
+
+namespace SistemaDePlanillas.App_Start
+{
+    public class InitialConfig
+    {
+        public static void init()
+        {
+            DBManager.getInstance();
+            SessionManager.getInstance();
+            ViewManager.getInstance();
+            Errors.getInstance();
+        }
+    }
+}
