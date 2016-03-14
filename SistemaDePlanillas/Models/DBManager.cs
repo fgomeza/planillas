f8c6f4e223af463822deb6ab8f27a8e48f4027a6
Francisco Gómez <fgomeza25@gmail.com>
2016-03-13 18:11:11 -0600

    Changed database name and password

--- SistemaDePlanillas/Models/DBManager.cs
+++ SistemaDePlanillas/Models/DBManager.cs
@@ -14,7 +14,6 @@ namespace SistemaDePlanillas.Models
         private static DBManager instance;
         private NpgsqlConnection cnx;
         private string stringConnect = "Server=localhost;Port=5432;Database=COOPESUPERACION;User Id=postgres;Password=root;";
-       // private string stringConnect = "Server=localhost;Port=5432;Database=planillas;User Id=postgres;Password=postgres;";
 
         private DBManager() { }
 
