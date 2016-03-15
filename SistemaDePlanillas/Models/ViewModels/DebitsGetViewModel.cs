b4d6d0053da4f7594b04cebdae3fa3cae4ac2aa5
JafetRoman <jafet21@hotmail.es>
2016-03-14 19:33:34 -0600

    sSas

--- SistemaDePlanillas/Models/ViewModels/DebitsGetViewModel.cs
+++ SistemaDePlanillas/Models/ViewModels/DebitsGetViewModel.cs
@@ -5,12 +5,20 @@ using System.Web;
 
 namespace SistemaDePlanillas.Models.ViewModels
 {
+    //Si una vista especifica no requiere un modelo puede ser omitido sin problemas
+
+    //Nombre debe ser GroupOperationViewModel
     public class DebitsGetViewModel
     {
+
+        //definir datos de vista
         public string nombre { get; set; }
+
+        //El constructor puede o no recibir el usuario
         public DebitsGetViewModel(User user)
         {
             nombre = user.Name;
+            //hacer lo que se quiera
         }
     }
 }
