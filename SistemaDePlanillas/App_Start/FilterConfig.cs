e29a60a4a189c37c56c8c940a1b4bfde40c75e38
Francisco Gómez <fgomeza25@gmail.com>
2016-03-14 22:00:23 -0600

    Setting up authentication and some razor markup

--- SistemaDePlanillas/App_Start/FilterConfig.cs
+++ SistemaDePlanillas/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace SistemaDePlanillas
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
