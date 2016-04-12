c5f4f290359fca930fe152ecf21ebced7ca45adb
JafetRoman <jafet21@hotmail.es>
2016-04-11 19:32:28 -0600

    dasd
    da

---
+++ misc/DB/pruebas.sql
@@ -0,0 +1,30 @@
+﻿INSERT INTO locations(
+            id, name, call_price)
+    VALUES (1,'tibas',500);
+
+INSERT INTO groups(
+            name, description, icon)
+    VALUES ('debits','/debits/','');
+
+INSERT INTO operations(
+            name, description, group_id)
+    VALUES ('debits/add','add/','debits');
+
+INSERT INTO operations(
+            name, description, group_id)
+    VALUES ('debits/delete','delete/','debits');
+    
+INSERT INTO operations(
+            name, description, group_id)
+    VALUES ('debits/update','update/','debits');
+
+
+INSERT INTO roles(
+            id, name, location)
+    VALUES (1,'Admin',1);
+
+INSERT INTO users(
+            id, name, email, username, password, role, location)
+    VALUES (1,'Jonn','xxx','JonnCh','123',1,1);
+
+select * from users where username = 'JonnCh' and password = '123';
