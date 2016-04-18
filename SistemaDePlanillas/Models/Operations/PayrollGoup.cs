c4f7df64ad9622d6bb7e73e23b74cc9fdf139fdd
JafetRoman <jafet21@hotmail.es>
2016-04-18 17:19:49 -0600

    imperative to funcional payroll calculation

--- SistemaDePlanillas/Models/Operations/PayrollGoup.cs
+++ SistemaDePlanillas/Models/Operations/PayrollGoup.cs
@@ -10,111 +10,80 @@ namespace SistemaDePlanillas.Models.Operations
     {
 
 
-        public static Response calculate(User user,DateTime initialDate, DateTime endDate)
+        public static Response calculate(User user, DateTime initialDate, DateTime endDate)
         {
-            List<object> rows= new List<object>();
             var employees = DBManager.Instance.selectAllActiveEmployees(user.Location).Detail;
             double totalPayroll = 0;
-            foreach (var employee in employees)
-            {
-                var calls = DBManager.Instance.callListByEmployee(employee.id,endDate).Detail;
-                long totalCalls = calls.Sum(c => c.calls);
-                var location = DBManager.Instance.getLocation(user.Location).Detail;
-                double callPrice = location.CallPrice;
-                var penaltiesDB = DBManager.Instance.selectAllPenalty(employee.id, endDate).Detail;
-                var penalties = penaltiesByEmployee(penaltiesDB);
-                double totalPenalties = penaltiesDB.Sum(p=>p.amount* p.penaltyPrice);
-                var extras = DBManager.Instance.selectExtras(employee.id).Detail;
-                double totalExtras = extras.Sum(e => e.amount);
-                var fixedDebitsDB = DBManager.Instance.selectDebits(employee.id).Detail;
-                var fixedDebits = fixedDebitsByEmployee(fixedDebitsDB);
-                double totalFixedDebits = fixedDebitsDB.Sum(d=>d.amount);
-                var paymentDebitsDB = DBManager.Instance.selectPaymentDebits(employee.id).Detail;
-                var paymentDebits = paymentDebitsByEmployee(paymentDebitsDB);
-                double totalPaymentDebits = paymentDebitsDB.Sum(d => (d.remainingAmount / d.missingPayments) + d.total * d.interestRate);
-                double grossAmount = (employee.salary/ 30.4167)*(endDate-initialDate).Days + (totalCalls * callPrice);
-                double saving = DBManager.Instance.selectSavingByEmployee(employee.id).Detail;
-                double netSalary = grossAmount + totalExtras - grossAmount * location.Capitalization - totalPenalties - totalPaymentDebits - totalFixedDebits - employee.negativeAmount;
-                rows.Add(new
-                {
-                    employee = employee.name,
-                    calls = new { amount = totalCalls, total = totalCalls * callPrice, callList = calls },
-                    penalties = penalties,
-                    extras = new { total = totalExtras, extraList = extras },
-                    fixedDebits = fixedDebits,
-                    paymentDebits = paymentDebits,
-                    salary = grossAmount,
-                    saving = new { amount = grossAmount * location.Capitalization, total = saving + grossAmount * location.Capitalization },
-                    netSalary = netSalary,
-                    negativeAmount = employee.negativeAmount
-                });
-                totalPayroll += netSalary > 0 ? netSalary : 0;
-            }
+            var rows = employees.Select(employee =>
+              {
+                  var calls = DBManager.Instance.callListByEmployee(employee.id, endDate).Detail;
+                  long totalCalls = calls.Sum(c => c.calls);
+                  var location = DBManager.Instance.getLocation(user.Location).Detail;
+                  double callPrice = location.CallPrice;
+                  var penaltiesDB = DBManager.Instance.selectAllPenalty(employee.id, endDate).Detail;
+                  var penalties = penaltiesByEmployee(penaltiesDB);
+                  double totalPenalties = penaltiesDB.Sum(p => p.amount * p.penaltyPrice);
+                  var extras = DBManager.Instance.selectExtras(employee.id).Detail;
+                  double totalExtras = extras.Sum(e => e.amount);
+                  var fixedDebitsDB = DBManager.Instance.selectDebits(employee.id).Detail;
+                  var fixedDebits = fixedDebitsByEmployee(fixedDebitsDB);
+                  double totalFixedDebits = fixedDebitsDB.Sum(d => d.amount);
+                  var paymentDebitsDB = DBManager.Instance.selectPaymentDebits(employee.id).Detail;
+                  var paymentDebits = paymentDebitsByEmployee(paymentDebitsDB);
+                  double totalPaymentDebits = paymentDebitsDB.Sum(d => (d.remainingAmount / d.missingPayments) + d.total * d.interestRate);
+                  double grossAmount = (employee.salary / 30.4167) * (endDate - initialDate).Days + (totalCalls * callPrice);
+                  double saving = DBManager.Instance.selectSavingByEmployee(employee.id).Detail;
+                  double netSalary = grossAmount + totalExtras - grossAmount * location.Capitalization - totalPenalties - totalPaymentDebits - totalFixedDebits - employee.negativeAmount;
 
-            return Responses.WithData(new { initialDate = initialDate, endDate = endDate, employees = rows, total = totalPayroll });
-
+                  totalPayroll += netSalary > 0 ? netSalary : 0;
+                  return new
+                  {
+                      employee = employee.name,
+                      calls = new { amount = totalCalls, total = totalCalls * callPrice, callList = calls },
+                      penalties = penalties,
+                      extras = new { total = totalExtras, extraList = extras },
+                      fixedDebits = fixedDebits,
+                      paymentDebits = paymentDebits,
+                      salary = grossAmount,
+                      saving = new { amount = grossAmount * location.Capitalization, total = saving + grossAmount * location.Capitalization },
+                      netSalary = netSalary,
+                      negativeAmount = employee.negativeAmount
+                  };
+              });
+            return Responses.WithData(rows);
         }
-
 
         private static List<object> paymentDebitsByEmployee(List<PaymentDebit> debitsByEmployee)
         {
-            Dictionary<string, List<PaymentDebit>> debitsByType = new Dictionary<string, List<PaymentDebit>>();
-            foreach (var debit in debitsByEmployee)
+            return debitsByEmployee.GroupBy(d => d.typeName, d => d, (k, g) => (object)new
             {
-                if (debitsByType.ContainsKey(debit.typeName))
-                    debitsByType[debit.typeName].Add(debit);
-                else
-                    debitsByType.Add(debit.typeName, new List<PaymentDebit>() { debit });
-            }
-            List<object> debits = new List<object>();
-            foreach (var debit in debitsByType)
-            {
-                debits.Add(new
-                {
-                    TypeName = debit.Key,
-                    amount = debit.Value.Count,
-                    total = debit.Value.Sum(d => (d.remainingAmount/d.missingPayments)+d.total*d.interestRate), 
-                    debitList = debit.Value
-                });
-            }
-            return debits;
+                TypeName = k,
+                amount = g.Count(),
+                total = g.Sum(d => (d.remainingAmount / d.missingPayments) + d.total * d.interestRate),
+                debitList = g
+            }).ToList();
         }
 
         private static List<object> fixedDebitsByEmployee(List<Debit> debitsByEmployee)
         {
-            Dictionary<string, List<Debit>> debitsByType = new Dictionary<string, List<Debit>>();
-            foreach (var debit in debitsByEmployee)
+            return debitsByEmployee.GroupBy(d => d.typeName, d => d, (k, g) => (object)new
             {
-                if (debitsByType.ContainsKey(debit.typeName))
-                     debitsByType[debit.typeName].Add(debit);
-                else
-                    debitsByType.Add(debit.typeName, new List<Debit>() { debit});
-            }
-            List<object> debits = new List<object>();
-            foreach(var debit in debitsByType)
-            {
-                debits.Add(new { TypeName = debit.Key, amount =debit.Value.Count, total = debit.Value.Sum(d=>d.amount), debitList = debit.Value});
-            }
-            return debits;
+                TypeName = k,
+                amount = g.Count(),
+                total = g.Sum(d => d.amount),
+                debitList = g
+            }).ToList();
         }
 
-        private static  List<object> penaltiesByEmployee(List<Penalty> penaltiesByEmployee)
+        private static List<object> penaltiesByEmployee(List<Penalty> penaltiesByEmployee)
         {
-            
-            Dictionary<string, List<Penalty>> penaltiesByType = new Dictionary<string, List<Penalty>>();
-            foreach (var penalty in penaltiesByEmployee)
+            return penaltiesByEmployee.GroupBy(p => p.typeName, p => p, (k, g) => (object)new
             {
-                if (penaltiesByType.ContainsKey(penalty.typeName))
-                    penaltiesByType[penalty.typeName].Add(penalty);
-                else
-                    penaltiesByType.Add(penalty.typeName, new List<Penalty>() { penalty });
-            }
-            List<object> penalties = new List<object>();
-            foreach (var penalty in penaltiesByType)
-            {
-                penalties.Add(new { typeName = penalty.Key, amount = penalty.Value.Sum(p => p.amount), total = penalty.Value.Sum(p => p.amount * p.penaltyPrice), typeList = penalty.Value });
-            }
-            return penalties;
+                typeName = k,
+                amount = g.Sum(p => p.amount),
+                total = g.Sum(p => p.amount * p.penaltyPrice),
+                typeList = g
+            }).ToList();
         }
     }
 
