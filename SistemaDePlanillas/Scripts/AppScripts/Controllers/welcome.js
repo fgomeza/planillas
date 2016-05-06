40254862d1e96c0e998b8b2d4e42af9bfec5641c
Francisco Gómez <fgomeza25@gmail.com>
2016-05-06 14:00:53 -0600

    Welcome screen

--- SistemaDePlanillas/Scripts/AppScripts/Controllers/welcome.js
+++ SistemaDePlanillas/Scripts/AppScripts/Controllers/welcome.js
@@ -1,10 +1,12 @@
-﻿define(['jquery', 'viewModels/title'], function ($, title) {
+﻿define(['jquery', 'knockout', 'viewModels/contacts', 'viewModels/title'], function ($, ko, vm, title) {
 
     function Controller() {
 
         this.init = function () {
 
             var $containerElement = $('#welcomeSection');
+
+            ko.applyBindings(vm, $containerElement[0]);
 
             $containerElement.parent().fadeIn();
 
