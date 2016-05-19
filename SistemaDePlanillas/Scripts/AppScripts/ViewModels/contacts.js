bb2c5297a4058904727e451a1f10721ad7349976
JonnCh <jonnch15j@outlook.com>
2016-05-18 21:20:50 -0600

    123

--- SistemaDePlanillas/Scripts/AppScripts/ViewModels/contacts.js
+++ SistemaDePlanillas/Scripts/AppScripts/ViewModels/contacts.js
@@ -6,7 +6,7 @@
 
         self.contacts = [
             { name: 'Francisco Gómez', email: 'fgomeza25@gmail.com' },
-            { name: 'Jonathan Charpentier', email: 'jcharpentier@mobilize.net' },
+            { name: 'Jonathan Charpentier', email: 'jonnch15j@outlook.com' },
             { name: 'Jafet Román', email: 'jafet21@hotmail.es' },
             { name: 'Oscar Chaves', email: 'andresch1009@gmail.com' },
             { name: 'Carlos Agüero', email: 'dev.aguero@gmail.com' },
